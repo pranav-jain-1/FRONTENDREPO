c6126a6361336070be63669540c988ead44cc952
pranav-jain-1 <jainpranav0606@gmail.com>
2025-07-01 12:18:51 +0530

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(</React.StrictMode
+
+4. **Click "Commit new file"**
+
+**Let me know when you've created both files, then I'll give you the next ones!** 
+
+We're building your EcoTracker app piece by piece! 🌱
